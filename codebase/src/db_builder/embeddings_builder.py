0fb6ec37adf4dd2bd23b43382c14766a57ecd284
Jeff Katzy <jeffreyerickatz@gmail.com>
2024-02-16 17:42:37 -0800

    remove solution

--- codebase/src/db_builder/embeddings_builder.py
+++ codebase/src/db_builder/embeddings_builder.py
@@ -9,10 +9,7 @@ def read_csv(file_name):
     return df
 
 def combine_data(df):
-    restaurants = df['Restaurant']
-    reviews = df['Review']
-    combined_series = 'restaurant name: ' + restaurants + ' Review: ' + reviews
-    return combined_series
+    pass
 
 def build_client():
     # api_key = ""
@@ -22,27 +19,10 @@ def build_client():
     return client
 
 def text_to_vectors(text_inputs):
-    client = build_client()
-    MODEL = "text-embedding-3-small"
-    res = client.embeddings.create(
-        input=text_inputs, model=MODEL
-    )
-    vectors = res.data
-    np_embeddings = [np.array(vector.embedding) for vector in vectors]
-    return np_embeddings
+    pass
 
 def build_embeddings_df_from(text_inputs):
-    embeddings = text_to_vectors(text_inputs)
-    df = pd.DataFrame(text_inputs, columns = ['text'])
-    df = df.assign(embedding = embeddings)
-    return df
+    pass
 
 def build_in_batches_from(combined_series, batch_size = 30):
-    dfs = []
-    cleaned_df = combined_series.dropna()
-    for i in range(0, len(cleaned_df), batch_size):
-        selected_df = cleaned_df.iloc[i:i+batch_size]
-        embeddings_df = build_embeddings_df_from(selected_df.to_list())
-        dfs.append(embeddings_df)
-    combined_embedded = pd.concat(dfs, axis = 0)
-    return combined_embedded
+    pass
--- codebase/src/llm/llm_model.py
+++ codebase/src/llm/llm_model.py
@@ -6,15 +6,7 @@ from db_builder.embeddings_builder import text_to_vector_df
 
 
 def build_context_from_distances_to(question, db_path):
-    q_embedding_array = text_to_vectors(question)[0]
-
-    df = pd.read_parquet(db_path)
-    embeddings = df.embedding.to_list()
-    distances = [cosine(q_embedding_array, embedding) for embedding in embeddings]
-    df['distances'] = distances
-    top_three = df.sort_values('distances')[:3].text
-    context = '\n\n'.join(top_three)
-    return context
+    pass
 
 def generate_prompt(question, context):
     return f'''Provide a 2-3 sentence answer to the question based on the following sources. Be original, concise, accurate, and helpful.
@@ -42,8 +34,4 @@ def get_answer(prompt, api_key):
     return response
 
 def question_and_answer(question, db_path):
-    context = build_context_from_distances_to(question, db_path)
-    prompt = generate_prompt(question, context)
-    api_key=""
-    answer = get_answer(prompt, api_key)
-    return answer.choices[0].message.content
+    pass
